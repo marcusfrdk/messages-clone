51db5f42bccb119a5dad972280fb04ae517365f2
Marcus Fredriksson <marcus@marcusfredriksson.com>
2020-12-10 19:34:37 +0100

    Generate new user and generate random sentence

---
+++ src/components/buttons/GenerateNewUserAndMessageButton.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import axios from 'axios';
+import { useGlobal, setGlobal } from 'reactn';
+
+// Type
+import Contact from '../../types/Contact';
+import Message from '../../types/Message';
+
+// Function
+import generateNewID from '../../functions/generateNewID';
+import generateRandomSentence from '../../functions/generateRandomSentance';
+
+const GenerateNewUserAndMessageButton = ({ setFirstModalActive, setMessages, messages }:Props) => {
+    const [ contacts ] = useGlobal<any>('contacts');
+
+    const generateNewMessage = (id:number, name:string):Message => {
+        let message:Message = {}
+
+        message.id = id;
+        message.message = generateRandomSentence();
+        message.name = name
+        message.sender = Math.random() > .5 ? id : 0;
+        message.receiver = message.sender == id ? 0 : id;
+        message.time = +new Date();
+        message.read = false
+        
+        return message;
+    }
+    
+    const generate = () => {
+        const uri = "https://randomuser.me/api"
+        let newUser:Contact = {};
+        
+        // Fetch data for new user
+        axios
+        .get(uri)
+        .then(res => {
+            let data = res.data.results[0]
+
+            newUser.id = generateNewID();
+            newUser.name = `${data.name.first} ${data.name.last}`;
+            newUser.phone = data.phone;
+            newUser.email = data.email;
+            newUser.gender = data.gender;
+            newUser.image = newUser.id < 100 ? Math.random() > .5 ? true : false : false;
+
+            let newMessage = generateNewMessage(newUser.id, newUser.name);
+            newUser.messages = [newMessage]; 
+
+            let newContacts = [...contacts, newUser];
+            setGlobal({ contacts: newContacts });
+
+            // Append to messages
+            let newList = [...messages, newMessage];
+            newList.sort((a, b) => {
+                return (a.time > b.time) ? -1 : 1;
+            })
+            
+            setMessages(newList)
+        })
+        .catch(err => console.log(err));
+
+
+        setFirstModalActive(false);
+    }
+
+    return (
+        <View style={{ padding: 16 }}>
+            <Button title="Generate new message" onPress={generate}/>
+        </View>
+    )
+}
+
+interface Props {
+    setFirstModalActive: any,
+    messages: any,
+    setMessages: any
+}
+
+export default GenerateNewUserAndMessageButton;
---
+++ src/functions/generateRandomSentance.ts
@@ -0,0 +1,27 @@
+            
+export default () => {
+    // Used https://codepen.io/chiragbhansali/pen/EWppvy?editors=0010
+
+    let verbs:string[], nouns:string[], adjectives:string[], adverbs:string[], preposition:string[];
+    nouns = ["bird", "clock", "boy", "plastic", "duck", "teacher", "old lady", "professor", "hamster", "dog"];
+    verbs = ["kicked", "ran", "flew", "dodged", "sliced", "rolled", "died", "breathed", "slept", "killed"];
+    adjectives = ["beautiful", "lazy", "professional", "lovely", "dumb", "rough", "soft", "hot", "vibrating", "slimy"];
+    adverbs = ["slowly", "elegantly", "precisely", "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately"];
+    preposition = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards"];
+
+    function sentence() {
+      var rand1 = Math.floor(Math.random() * 10);
+      var rand2 = Math.floor(Math.random() * 10);
+      var rand3 = Math.floor(Math.random() * 10);
+      var rand4 = Math.floor(Math.random() * 10);
+      var rand5 = Math.floor(Math.random() * 10);
+      var rand6 = Math.floor(Math.random() * 10);
+      //                var randCol = [rand1,rand2,rand3,rand4,rand5];
+      //                var i = randGen();
+      var content = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + preposition[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + ".";
+
+      return content;
+    }
+
+    return sentence()
+}            
