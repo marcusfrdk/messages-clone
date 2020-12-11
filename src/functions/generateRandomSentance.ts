e13049194401b9593ea8dbf4c98d21c3113422f0
Marcus Fredriksson <marcus@marcusfredriksson.com>
2020-12-11 08:34:26 +0100

    Random string generator -> Select from list of sentences

--- src/functions/generateRandomSentance.ts
+++ src/functions/generateRandomSentance.ts
@@ -1,27 +1,9 @@
-            
+import sentences from '../data/sentences.json';
+
 export default () => {
-    // Used https://codepen.io/chiragbhansali/pen/EWppvy?editors=0010
+  const length = sentences.length;
+  const index = Math.floor(Math.random() * length)
+  const message = sentences[index]
 
-    let verbs:string[], nouns:string[], adjectives:string[], adverbs:string[], preposition:string[];
-    nouns = ["bird", "clock", "boy", "plastic", "duck", "teacher", "old lady", "professor", "hamster", "dog"];
-    verbs = ["kicked", "ran", "flew", "dodged", "sliced", "rolled", "died", "breathed", "slept", "killed"];
-    adjectives = ["beautiful", "lazy", "professional", "lovely", "dumb", "rough", "soft", "hot", "vibrating", "slimy"];
-    adverbs = ["slowly", "elegantly", "precisely", "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately"];
-    preposition = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards"];
-
-    function sentence() {
-      var rand1 = Math.floor(Math.random() * 10);
-      var rand2 = Math.floor(Math.random() * 10);
-      var rand3 = Math.floor(Math.random() * 10);
-      var rand4 = Math.floor(Math.random() * 10);
-      var rand5 = Math.floor(Math.random() * 10);
-      var rand6 = Math.floor(Math.random() * 10);
-      //                var randCol = [rand1,rand2,rand3,rand4,rand5];
-      //                var i = randGen();
-      var content = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + preposition[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + ".";
-
-      return content;
-    }
-
-    return sentence()
-}            
+  return message;
+}
